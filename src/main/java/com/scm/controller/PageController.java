c8ba2bff2ec39727cd605d1c0b81ba6f6f33678a
pranshu <pranshukabra27@gmail.com>
2024-09-26 00:51:40 +0530

    Navbar Integration with tailwind and formatting of the code

--- src/main/java/com/scm/controller/PageController.java
+++ src/main/java/com/scm/controller/PageController.java
@@ -2,7 +2,9 @@ package com.scm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class PageController {
@@ -31,4 +33,19 @@ public class PageController {
         return "services";
     }
 
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+    
 }

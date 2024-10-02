c5c4da8e6ad32759bfdd7b0f81d3ac8b60faa158
pranshu <pranshukabra27@gmail.com>
2024-10-02 20:01:41 +0530

    Starting with spring security

---
+++ src/main/java/com/scm/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    // user dashboard page
+    @GetMapping("dashboard")
+    public String userDashboard() {
+        return "user/dashboard";
+    }
+
+    // user profile page
+    @GetMapping("profile")
+    public String userProfile() {
+        return "user/profile";
+    }
+
+    // user add contacts page
+    // user view contacts page
+    // user edit contact
+    // user delete contact    
+}
